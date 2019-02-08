9bbcfa1ebb7ee7b3bfd84f3bef2f99d683aacd2c
Dedy Yugo Purwanto <dedy.yugo.purwanto@gmail.com>
2019-02-08 16:18:55 +0700

    Make sure page data is array

--- src/Services/BloggerApi.php
+++ src/Services/BloggerApi.php
@@ -308,7 +308,7 @@ class BloggerApi
                 return $item;
             });
 
-            $pages['items'] = $items;
+            $pages['items'] = $items->toArray();
         }
 
         return $pages;
